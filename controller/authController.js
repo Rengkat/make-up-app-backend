6d933dd0784a42f6aa3681b86b799f57edb911c6
Rengkat <successinmotion11@gmail.com>
2024-08-07 13:03:35 +0100

    auth route added

---
+++ controller/authController.js
@@ -0,0 +1,4 @@
+const registerUser = (req, res) => {};
+const loginUser = (req, res) => {};
+const logoutUser = (req, res) => {};
+module.exports = { registerUser, loginUser, logoutUser };
--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require("express");
 const app = express();
 
 //rest of packages importations
+const morgan = require("morgan");
 
 //database
 const connectDB = require("./db/connectDB");
@@ -11,19 +12,22 @@ const connectDB = require("./db/connectDB");
 //routes importations
 
 //middlewares importations
-
+const notFoundMiddleware = require("./middleware/note-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 //middlewares initialization
+app.use(morgan("tiny")); //to see the
 
 //route initialization
 
 //errors initialization
-
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 //starting the app
 const port = process.env.PORT || 5000;
 const start = async () => {
   try {
     await connectDB(process.env.MONGODB_URL);
-    console.log(`Server is running on port ${port}...`);
+    app.listen(port, () => console.log(`Server is running on port ${port}...`));
   } catch (error) {
     console.log(error.message);
   }
