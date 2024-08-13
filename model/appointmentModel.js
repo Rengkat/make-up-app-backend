a47d4a3eeb49ac6fc1cd163180deb639fed728a7
Rengkat <successinmotion11@gmail.com>
2024-08-13 05:59:33 +0100

    category added

--- model/appointmentModel.js
+++ model/appointmentModel.js
@@ -3,6 +3,7 @@ const AppointmentSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     required: true,
+    ref: "User",
   },
   date: {
     type: Date,
---
+++ model/categoryModel.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please provide a category name"],
+      unique: true,
+      trim: true,
+    },
+    products: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+CategorySchema.index({ name: 1 });
+
+const Category = mongoose.model("Category", CategorySchema);
+
+module.exports = Category;
