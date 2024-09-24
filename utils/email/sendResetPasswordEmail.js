2c5f720253421a090e2d79b7515c443d1e639a95
Rengkat <successinmotion11@gmail.com>
2024-09-25 00:40:22 +0100

    email template added

--- utils/email/sendResetPasswordEmail.js
+++ utils/email/sendResetPasswordEmail.js
@@ -2,15 +2,67 @@ const sendEmail = require("./sendEmail");
 
 const sendResetPasswordEmail = async ({ firstName, email, resetPasswordToken, origin }) => {
   const link = `${origin}/auth/reset-password?resetToken=${resetPasswordToken}&email=${email}`;
-  const message = `<p>Kindly reset your password by clicking the link below</p>
-    <a href=${link}>Reset Password</a>
-    <p> If you are not the person that made this request, kindly ignore</p>
-    `;
+  const html = `
+    <body
+    style="
+      padding: 0;
+      margin: 0;
+      background-color: #f4f4f4;
+      width: 100%;
+    ">
+      <table width="100%" cellpadding="0" cellspacing="0" border="0" style="max-width: 600px; margin: auto;">
+        <tr>
+          <td style="padding: 0;">
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border: 1px solid #dddddd; background: #fffbf1;">
+              <tr>
+                <td style="background: #dd6f1d; padding: 1.5rem; text-align: center;">
+                  <a href="${origin}" style="display: inline-block; color: white; text-decoration: none; font-size: larger;">
+                    FULLBEAUTY
+                  </a>
+                </td>
+              </tr>
+              <tr>
+                <td style="padding: 1.5rem;">
+                  <p style="font-size: 16px; line-height: 1.5;">Hello ${firstName},</p>
+                  <p style="font-size: 16px; line-height: 1.5;">
+                    We received a request to reset your password. Please click the button below to reset it:
+                  </p>
+                  <div style="text-align: center; margin: 20px 0;">
+                    <a
+                      href="${link}"
+                      style="
+                        background: #c86011;
+                        border-radius: 7px;
+                        padding: 0.8rem 1.2rem;
+                        color: white;
+                        font-size: 16px;
+                        text-decoration: none;
+                        display: inline-block;
+                      ">
+                      Reset Your Password
+                    </a>
+                  </div>
+                  <p style="font-size: 14px; line-height: 1.5;">
+                    If you didn't request this email, please ignore it or contact support if you have any concerns.
+                  </p>
+                  <p style="font-size: 14px; line-height: 1.5;">
+                    Regards, <br />
+                    Fullbeauty
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>
+  `;
+
   return sendEmail({
     to: email,
-    subject: "Reset Password",
-    html: `<h4>Hey ${firstName}</h4>,
-        ${message}`,
+    subject: "Reset Your Password",
+    html,
   });
 };
+
 module.exports = sendResetPasswordEmail;
--- utils/email/sendVerificationEmail.js
+++ utils/email/sendVerificationEmail.js
@@ -1,18 +1,65 @@
 const sendEmail = require("./sendEmail");
-
 const sendVerificationEmail = async ({ firstName, email, origin, verificationToken }) => {
   const link = `${origin}/auth/verify-email?verificationToken=${verificationToken}&email=${email}`;
-  const message = `
-    <p>Please confirm your email address by clicking the link below:</p>
-    <a href="${link}" style="padding: 0.5rem 2rem; background: purple; border-radius: 7px; color: white; text-decoration: none;">Verify your email address</a>
-    <p>If you did not sign up for a Fullybeauty account, you can simply disregard this email.</p>
-    <p>Enjoy our services</p>
+  const html = `
+    <body
+    style="
+      padding: 0;
+      margin: 0;
+      background-color: #f4f4f4;
+      width: 100%;
+    ">
+      <table width="100%" cellpadding="0" cellspacing="0" border="0" style="max-width: 600px; margin: auto;">
+        <tr>
+          <td style="padding: 0;">
+            <table width="100%" cellpadding="0" cellspacing="0" border="0" style="border: 1px solid #dddddd; background: #fffbf1;">
+              <tr>
+                <td style="background: #dd6f1d; padding: 1.5rem; text-align: center;">
+                  <a href="${origin}" style="display: inline-block;color: white; text-decoration: none; font-size: larger">
+          FULLBEAUTY</a>
+                </td>
+              </tr>
+              <tr>
+                <td style="padding: 1.5rem;">
+                  <p style="font-size: 16px; line-height: 1.5;">Hello ${firstName},</p>
+                  <p style="font-size: 16px; line-height: 1.5;">
+                    Welcome to Fullbeauty. Please verify your email by clicking on the link below.
+                  </p>
+                  <div style="text-align: center; margin: 20px 0;">
+                    <a
+                      href="${link}"
+                      style="
+                        background: #c86011;
+                        border-radius: 7px;
+                        padding: 0.8rem 1.2rem;
+                        color: white;
+                        font-size: 16px;
+                        text-decoration: none;
+                        display: inline-block;
+                      ">
+                      Verify Your Email Address
+                    </a>
+                  </div>
+                  <p style="font-size: 14px; line-height: 1.5;">
+                    If you didn't request this email, please ignore it.
+                  </p>
+                  <p style="font-size: 14px; line-height: 1.5;">
+                    Regards, <br />
+                    Fullbeauty
+                  </p>
+                </td>
+              </tr>
+            </table>
+          </td>
+        </tr>
+      </table>
+    </body>
   `;
 
   return sendEmail({
     subject: "Email Verification",
     to: email,
-    html: `<h4>Hello ${firstName},</h4>${message}`,
+    html,
   });
 };
 
